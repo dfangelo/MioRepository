55a0191d5cf1249ca006ec6da0b3ece5e9ded443
adifazio <a.difazio@widegroup.ch>
2015-02-04 15:31:36 +0100

    MailManager

---
+++ MvcHubRepository/Mvc_Study/Mvc_Study/Models/MailManager/Mail.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+using MaiMvc.Models.MailManager.Mail;
+using System.Configuration;
+using System.Net.Mime;
+
+namespace MaiMvc.Models
+{
+    public class Mail
+    {
+        #region Property
+        /// <summary>
+        /// Definiamo tutte le proprietà che deve avere la nostra Mail
+        /// </summary>
+        Message message = null;
+
+        string smtpHost = string.Empty;
+        int port = 25;
+
+        // Chi invia la mail
+        string mailSender = string.Empty;
+
+        // Lista di destinatari (TO)
+        List<string> lstDestinatari = null;
+
+        // Lista Copia Conoscenza (CC)
+        List<string> lstCc = null; 
+        #endregion
+
+        
+        public Mail(Message message, string smtpHost, string mailSender, int port,
+            List<string> lstTo, List<string> lstCc)
+        {
+            // La parola chiave THIS si riferisce all'istanza corrente della classe e viene anche utilizzata 
+            // come modificatore del primo parametro di un metodo di estensione
+            this.message = message;
+            this.lstDestinatari = lstTo;
+            this.lstCc = lstCc;
+
+            this.mailSender = mailSender;
+            this.smtpHost = smtpHost;
+            this.port = port;
+
+            //CONFIGURATION MANAGER, ottiene i dati di AppSettingsSection per la configurazione predefinita dell'applicazione corrente
+            if (port == 0)
+                this.mailSender = ConfigurationManager.AppSettings.Get("Port");
+
+            if (mailSender.Length == 0)
+                this.mailSender = ConfigurationManager.AppSettings.Get("mailSender");
+
+            if (smtpHost.Length == 0)
+                this.smtpHost = ConfigurationManager.AppSettings.Get("smtpHost");
+        }
+
+        /// <summary>
+        /// Invia la mail
+        /// AlternateView: Rappresenta il formato in cui visualizzare un messaggio di posta elettronica.
+        /// </summary>
+        public void Send()
+        {
+            try
+            {
+                AlternateView avHtml = AlternateView.CreateAlternateViewFromString(message.Body, null, MediaTypeNames.Text.Html);
+
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(this.mailSender);
+                mail.IsBodyHtml = true;
+                mail.Priority = MailPriority.Normal;
+                mail.Subject = message.Title;
+                mail.Body = message.Body;
+
+                if (this.lstDestinatari != null)
+                    foreach (string s in this.lstDestinatari)
+                        mail.To.Add(s);
+
+                if (this.lstCc != null)
+                    foreach (string s in this.lstCc)
+                        mail.CC.Add(s);
+
+                for (int i = 0; i < message.ImageList.Count; i++)
+                {
+                    string s = message.ImageList[i];
+                    var inlineLogo = new LinkedResource(s, MediaTypeNames.Image.Gif);
+                    inlineLogo.ContentId = string.Format("[IMAGE{0}]", i);
+
+                    avHtml.LinkedResources.Add(inlineLogo);
+                }
+                mail.AlternateViews.Add(avHtml);
+
+                SmtpClient smtp = new SmtpClient(this.smtpHost, this.port);
+                smtp.Send(mail);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ MvcHubRepository/Mvc_Study/Mvc_Study/Models/MailManager/Message.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MaiMvc.Models.MailManager.Mail
+{
+    //PUBLIC, una routine diventa accessibile da tutte le istanze della classe.
+    public class Message
+    {
+        /// <summary>
+        /// definiamo le proprietà per il nostro messaggio
+        /// </summary>
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public List<string> ImageList { get; set; }
+
+        //PRIVATE, impedisce che la routine sia visibile al di fuori della classe di in cui è definita.
+        private string Template { get; set; }
+
+        // COSTRUTTORE ha lo stesso nome della classe e non restituisce alcun valore 
+
+        /// <COSTRUTTORE>
+        /// Il costruttore è COSTRUTTORE per impostare le proprietà che la classe deve avere al momento della sua creazione: 
+        /// nel nostro esempio, quando si crea una istanza della classe Message, le sue variabili 
+        /// Title, Body, ImageList e Template assumono i valori specificati nel relativo argomento del costruttore.
+        /// </COSTRUTTORE>
+        /// <param name="title"></param>
+        /// <param name="body"></param>
+        /// <param name="templateHTML"></param>
+        public Message(string title, string message, string templateHTML)
+        {
+            this.Title = title;
+            this.Template = templateHTML;
+            this.Body = CreateBody(message);
+        }
+
+        public Message(string title, string message, string templateHTML, List<string> imageList)
+        {
+            this.Title = title;
+            this.Template = templateHTML;
+            this.Body = CreateBody(message);
+            this.ImageList = imageList;
+        }
+
+        /// <summary>
+        /// Costruisce il messaggio in base al template
+        /// </summary>
+        /// <param name="body"></param>
+        /// <returns></returns>
+        public string CreateBody(string body)
+        {
+            string temp = this.Template;
+
+            temp = temp.Replace("[TITLE]", this.Title);
+            temp = temp.Replace("[MESSAGE]", body);
+
+            return temp;
+        }
+    }
+}
