f611e2c3368a1dd7ca0833be914cbe124b0270da
adifazio <a.difazio@widegroup.ch>
2015-02-04 18:02:13 +0100

    T_UserController

---
+++ MvcHubRepository/Mvc_Study/Mvc_Study/Controllers/T_UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Study.Controllers
+{
+    public class T_UserController : Controller
+    {
+        
+        // GET: T_User
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
